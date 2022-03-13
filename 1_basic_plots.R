4ca21c94098a17186bff1e49abbca50038f3df8f
Daniel <daniel.l.m.2442.123@gmail.com>
2022-03-13 12:02:16 +0100

    basic_plots

--- 1_basic_plots.R
+++ 1_basic_plots.R
@@ -8,12 +8,17 @@ summary(cheddar)
 # Histogramas de todas las variables
 
 ids = names(cheddar)
-layout(matrix(1:4, nrow = 1))
+m = dim(cheddar)[2]
+
+
+#######   HISTOGRAM 1   ############
+layout(matrix(1:m, nrow = 1))
 y_lab_string = "Quantity";
 for (id in ids){
   hist(cheddar[,id], xlab=id, ylab=y_lab_string, main = paste("Histogram of ", id))
   y_lab_string="";
 }
+<<<<<<< Updated upstream
 cheddar[c("taste")]
 
 
@@ -40,3 +45,40 @@ plot(H, T, main = "Relación entre Taste y H2S",
 plot(L, T, main = "Relación entre Taste y Lactic",
      xlab = "Lactic", ylab = "Taste",
      pch = 19, frame = FALSE)
+=======
+
+
+#######   BOX PLOT 1 ############
+layout(matrix(1:m, nrow = 1))
+y_lab_string = "Quantity";
+for (id in ids){
+  boxplot(cheddar[,id], ylab = y_lab_string, xlab=id, varwidth = TRUE)
+  y_lab_string="";
+}
+
+
+###### LINEAR REGRESSION 1 ############
+#par(mar = c(2,2,2,2))
+layout(matrix(1:(m*m), nrow = m))
+i=0;j=0;
+for (id_x in ids){
+  for (id_y in ids){
+    if (i==0){
+      y_lab_string = id_y
+    }else{
+      y_lab_string = "";
+    }
+    
+    if (j==0){
+      x_lab_string = id_x
+    }else{
+      x_lab_string = "";
+    }
+    
+    plot(cheddar[,id_x],cheddar[,id_y], ylab=y_lab_string, xlab=x_lab_string)
+    j=j+1;  
+  }
+  i=1+1;
+}
+
+>>>>>>> Stashed changes
