0fd6af869b64d4cb94943929db98cfe00db2ad8c
Rodrigo de la Nuez Moraleda <71011156+mondovi@users.noreply.github.com>
2022-03-21 22:50:53 +0100

    Update cheddar_script.R

--- modelo_completo/cheddar_script.R
+++ modelo_completo/cheddar_script.R
@@ -3,6 +3,38 @@ library(faraway)
 library(car)
 
 
+# Histogramas de todas las variables
+
+ids = names(cheddar)
+layout(matrix(1:4, nrow = 1))
+y_lab_string = "Quantity";
+for (id in ids){
+  hist(cheddar[,id], xlab=id, ylab=y_lab_string, main = paste("Histogram of ", id))
+  y_lab_string="";
+}
+cheddar[c("taste")]
+
+
+# Gráficos de las relaciones entre todas las variables.
+
+plot(cheddar)
+
+# Gráficos de dispersión entre la variable respuesta "Taste" y
+#  las variables predictoras "Acetic", "H2S" y "Lactose".
+
+plot(Acetic, taste, main = "Relación entre Taste y Acetic",
+     xlab = "Acetic", ylab = "Taste",
+     pch = 20, frame = FALSE)
+
+plot(H2S, taste, main = "Relación entre Taste y H2S",
+     xlab = "H2S", ylab = "Taste",
+     pch = 20, frame = FALSE)
+
+plot(Lactic, taste, main = "Relación entre Taste y Lactic",
+     xlab = "Lactic", ylab = "Taste",
+     pch = 19, frame = FALSE)
+
+
 # 1) Introducción
 
 data(cheddar)
